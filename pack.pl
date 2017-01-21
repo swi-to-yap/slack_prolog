75c7d8b594e4144f031b4a6367c6f9af7a485fff
logicmoo <logicmoo@gmail.com>
2017-01-21 14:01:21 -0800

    Initial commit of slack_client module.. Yap coming next

---
+++ pack.pl
@@ -0,0 +1,10 @@
+name(slack_prolog).
+version('0.0.1').
+title('Prolog interface to Slack http://www.slack.com').
+keywords([chat,bots,slack]).
+author( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
+packager( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
+maintainer( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
+home( 'https://github.com/swi-to-yap/slack_prolog' ).
+download( 'https://github.com/swi-to-yap/slack_prolog/release/*.zip' ).
+provides(slack_client).
